0cb71cb11798943f02b7e9284402a8985272d7a0
Lennon-Araujo <lennon_hih@hotmail.com>
2022-04-05 22:18:38 -0300

    implementando pool para remoção do usuário antes da execução de teste de cadastro

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -5,12 +5,27 @@ it('deve cadastrar um usuáro', () => {
   const email = "lennon@sbs.com";
   const password = "pwd123";
 
+  cy.task('removeUser', email).then(result => {
+    console.log(result)
+  })
+
   cy.visit('/signup')
 
   cy.get('input[placeholder="Nome"]').type(name)
   cy.get('input[placeholder="E-mail"]').type(email)
   cy.get('input[placeholder="Senha"]').type(password)
+  
+  // cy.intercept('POST', '/users', {
+  //   statusCode: 200
+  // }).as('postUser')
 
-  cy.contains('button', 'Cadastrar')
+  cy.contains('button', 'Cadastrar').click()
+
+  // cy.wait('@postUser')
+
+  cy.get('.toast')
+    .should('be.visible')
+    .find('p')
+    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
 
 })

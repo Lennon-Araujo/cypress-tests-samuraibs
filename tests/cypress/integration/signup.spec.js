356bfe33ca134030a379f3d11b03191d9c01c6e4
Lennon-Araujo <lennon_hih@hotmail.com>
2022-05-25 21:56:10 -0300

    adicionando teste de validação - email já cadastrado

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,31 +1,70 @@
 /// <reference types = "Cypress" />
 
-it('deve cadastrar um usuáro', () => {
-  const name = "Lennon Araujo";
-  const email = "lennon@sbs.com";
-  const password = "pwd123";
+describe('SignUp', () => {
+  it('deve cadastrar um usuáro', () => {
+    const user = {
+      name: "Lennon Araujo",
+      email: "lennon@sbs.com",
+      password: "pwd123"
+    }
 
-  cy.task('removeUser', email).then(result => {
-    console.log(result)
+    cy.task('removeUser', user.email).then(result => {
+      console.log(result)
+    })
+
+    cy.visit('/signup')
+
+    cy.get('input[placeholder="Nome"]').type(user.name)
+    cy.get('input[placeholder="E-mail"]').type(user.email)
+    cy.get('input[placeholder="Senha"]').type(user.password)
+
+    // cy.intercept('POST', '/users', {
+    //   statusCode: 200
+    // }).as('postUser')
+
+    cy.contains('button', 'Cadastrar').click()
+
+    // cy.wait('@postUser')
+
+    cy.get('.toast')
+      .should('be.visible')
+      .find('p')
+      .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+
   })
 
-  cy.visit('/signup')
+  it('deve apresentar mensagem de erro - usuário já cadastrado', () => {
 
-  cy.get('input[placeholder="Nome"]').type(name)
-  cy.get('input[placeholder="E-mail"]').type(email)
-  cy.get('input[placeholder="Senha"]').type(password)
-  
-  // cy.intercept('POST', '/users', {
-  //   statusCode: 200
-  // }).as('postUser')
+    const user = {
+      name: "Usuario teste",
+      email: "userteste@sbs.com",
+      password: "pwd123",
+      is_provider: true
+    }
 
-  cy.contains('button', 'Cadastrar').click()
+    cy.task('removeUser', user.email).then(result => {
+      console.log(result)
+    })
 
-  // cy.wait('@postUser')
+    cy.request(
+      'POST',
+      'http://localhost:3333/users',
+      user
+    ).then(response => {
+      expect(response.status).to.be.equal(200)
+    })
 
-  cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+    cy.visit('/signup')
 
+    cy.get('input[placeholder="Nome"]').type(user.name)
+    cy.get('input[placeholder="E-mail"]').type(user.email)
+    cy.get('input[placeholder="Senha"]').type(user.password)
+
+    cy.contains('button', 'Cadastrar').click()
+
+    cy.get('.toast')
+      .should('be.visible')
+      .find('p')
+      .should('have.text', 'Email já cadastrado para outro usuário.')
+  })
 })
